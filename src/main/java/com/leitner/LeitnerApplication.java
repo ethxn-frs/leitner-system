180a24c327609ccfa4bef5fc26f8e9c85cf5f61f
Ethan <ethanfrancois0@gmail.com>
2025-02-03 08:57:54 +0100

    Initial commit

---
+++ src/main/java/com/leitner/LeitnerApplication.java
@@ -0,0 +1,13 @@
+package com.leitner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leitner/LeitnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leitner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
